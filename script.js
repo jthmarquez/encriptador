168ab003357203237d6db4f7a37c1bb4a7e2de29
jthmarquez <jthmarquez@gmail.com>
2023-01-01 22:53:52 -0300

    finalizando encriptador

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-e --> enter
-o --> ober 
-i --> imes 
-a --> ai 
-u --> ufat 
+//e --> enter
+//o --> ober 
+//i --> imes 
+//a --> ai 
+//u --> ufat 
 
 function encriptar() {
     var texto = document.getElementById("inputTexto").value.toLowerCase();
@@ -17,9 +17,36 @@ function encriptar() {
     var txtCifrado = txtCifrado.replace(/a/igm,"ai");
     var txtCifrado = txtCifrado.replace(/u/igm,"ufat");
 
-    document.getElementById("imgDer").style.display = none;
-    document.getElementById("texto").style.display = none;
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
     document.getElementById("texto2").innerHTML = txtCifrado; 
     document.getElementById("copiar").style.display = "show";
     document.getElementById("copiar").style.display = "inherit";
+}
+
+function desencriptar() {
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+
+    //la i es para que solo afecte a mayusculas y minusculas -- e y E {
+    //la g es para toda la linea u oracion
+    //la m es para que afecte multiples lineas o parrafos }
+
+    var txtCifrado = texto.replace(/enter/igm,"e");
+    var txtCifrado = txtCifrado.replace(/ober/igm,"o");
+    var txtCifrado = txtCifrado.replace(/imes/igm,"i");
+    var txtCifrado = txtCifrado.replace(/ai/igm,"a");
+    var txtCifrado = txtCifrado.replace(/ufat/igm,"u");
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado; 
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+}
+
+function copy() {
+    var contenido = document.querySelector("#texto2");
+    contenido.select();
+    document.execCommand("copy", false,);
+    alert("¡Copiado!");
 }
